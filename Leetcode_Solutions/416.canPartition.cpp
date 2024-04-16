d20e50bc86c4de2a7a37fe024328f2cf9bc706fa
Jackin Lin <1410524125@qq.com>
2024-04-16 10:20:36 +0800

    Update 416.canPartition.cpp

--- Leetcode_Solutions/416.canPartition.cpp
+++ Leetcode_Solutions/416.canPartition.cpp
@@ -20,9 +20,7 @@ public:
         for (int i = 0; i < nums.size(); i++)
             for (int j = target; j >= nums[i]; j--)
                 dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
-        if (dp[target] == target)
-            return true;
-        return false;
+        return dp[target] == target;
     }
 };
 // @lc code=end
